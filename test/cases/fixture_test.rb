3179b60dc0888d40e1ea71a5602c1b8e715d75a4
Manfred Stienstra <manfred@fngtps.com>
2020-12-01 18:06:12 +0100

    [Rubocop] Use JSON.parse over JSON.load.

--- test/cases/fixture_test.rb
+++ test/cases/fixture_test.rb
@@ -13,7 +13,7 @@ module Fitments
     end
 
     def test_coerces_a_person_to_json
-      attributes = JSON.load(load_fixture(:person).to_json)
+      attributes = JSON.parse(load_fixture(:person).to_json)
       assert_equal %w[name started_at], attributes.keys
     end
 
